3b98230192918b3c5f5b402a5c02025960a64e77
Mina Magdy <phoenix.mena99@gmail.com>
2023-04-21 20:36:03 +0200

    add audio locally and modify script

--- battery-full-notification.sh
+++ battery-full-notification.sh
@@ -7,8 +7,8 @@ do
 	then
 		if (($battery_percent >= 95))
 		then
-			notify-send -i "$(pwd)/full-battery.png" "Battery full." "Level: ${battery_percent}%"
-			paplay /usr/share/sounds/Pop/stereo/notification/complete.oga
+			notify-send -i "$PWD/full-battery.png" "Battery full." "Level: ${battery_percent}%"
+			paplay $PWD/complete.oga
 		fi
 	fi
 	sleep 5m
